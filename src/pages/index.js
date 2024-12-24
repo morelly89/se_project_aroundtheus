2f64e5549b580af925578818b976967b4dc00000
morelly89 <morelly89@yahoo.com>
2024-12-24 15:42:46 -0500

    rename folder, disable add submit button after submitting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import { initialCards, validationSettings } from "../../util/constants.js";
 import "../pages/index.css";
-import Card from "../scripts/Card.js";
-import FormValidator from "../scripts/FormValidator.js";
-import PopupWithForm from "../scripts/PopupWithForm.js";
-import PopupWithImage from "../scripts/PopupWithImage.js";
-import Section from "../scripts/Section.js";
-import UserInfo from "../scripts/UserInfo.js";
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import Section from "../components/Section.js";
+import UserInfo from "../components/UserInfo.js";
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const editProfileCloseBtn = document.querySelector("#edit-close-button");
@@ -93,6 +93,7 @@ function handleCardClick(cardData) {
 /* -------------------------------------------------------------------------- */
 
 addButton.addEventListener("click", () => {
+  addCardFormValidator.disableSubmitButton();
   addCardModal.open();
 });
 
